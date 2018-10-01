13f1d9f9e2eaac401fde148a507a9a7c291fedc1
Vadim Akulin <vadimakulin@gmail.com>
2018-10-01 11:13:34 +0300

    инкапсулировал form.js

--- js/form.js
+++ js/form.js
@@ -1,75 +1,76 @@
 'use strict';
+(function () {
+  var form = document.querySelector('.ad-form');
+  var typeInput = form.querySelector('#type');
+  var priceInput = form.querySelector('#price');
+  var timeInInput = form.querySelector('#timein');
+  var timeOutInput = form.querySelector('#timeout');
+  var roomNumberInput = form.querySelector('#room_number');
+  var capacityInput = form.querySelector('#capacity');
 
-var form = document.querySelector('.ad-form');
-var typeInput = form.querySelector('#type');
-var priceInput = form.querySelector('#price');
-var timeInInput = form.querySelector('#timein');
-var timeOutInput = form.querySelector('#timeout');
-var roomNumberInput = form.querySelector('#room_number');
-var capacityInput = form.querySelector('#capacity');
-
-var limitationPrice = function () {
-  switch (typeInput.value) {
-    case 'flat':
-      priceInput.placeholder = window.card.types.flat.minPrice;
-      priceInput.min = window.card.types.flat.minPrice;
-      break;
-    case 'house':
-      priceInput.placeholder = window.card.types.house.minPrice;
-      priceInput.min = window.card.types.house.minPrice;
-      break;
-    case 'palace':
-      priceInput.placeholder = window.card.types.palace.minPrice;
-      priceInput.min = window.card.types.palace.minPrice;
-      break;
-    default:
-      priceInput.placeholder = window.card.types.bungalo.minPrice;
-      priceInput.min = window.card.types.bungalo.minPrice;
-  }
-};
-limitationPrice();
-
-var onTypeInputChange = function () {
-  limitationPrice();
-};
-typeInput.addEventListener('change', onTypeInputChange);
-
-var syncTime = function (changeElem, repeatElem) {
-  repeatElem.value = changeElem.value;
-};
-
-var onTimeInInputChange = function () {
-  syncTime(timeInInput, timeOutInput);
-};
-timeInInput.addEventListener('change', onTimeInInputChange);
-
-var onTimeOutInputChange = function () {
-  syncTime(timeOutInput, timeInInput);
-};
-timeOutInput.addEventListener('change', onTimeOutInputChange);
-
-var limitationCapacity = function () {
-  var roomInputValue = roomNumberInput.value;
-  var capacityOptions = capacityInput.querySelectorAll('option');
-
-  var disableCapacityOptions = function (value) {
-    var HUNDRED_ROOMS = '100';
-    var NOT_FOR_GUESTS = '0';
-    var roomValue = value === HUNDRED_ROOMS ? NOT_FOR_GUESTS : value;
-    for (var j = 0; j < capacityOptions.length; j++) {
-      var flag = true;
-      if ((capacityOptions[j].value > NOT_FOR_GUESTS && capacityOptions[j].value <= roomValue) || (roomValue === NOT_FOR_GUESTS && capacityOptions[j].value === roomValue)) {
-        flag = false;
-      }
-      capacityOptions[j].disabled = flag;
+  var limitationPrice = function () {
+    switch (typeInput.value) {
+      case 'flat':
+        priceInput.placeholder = window.card.types.flat.minPrice;
+        priceInput.min = window.card.types.flat.minPrice;
+        break;
+      case 'house':
+        priceInput.placeholder = window.card.types.house.minPrice;
+        priceInput.min = window.card.types.house.minPrice;
+        break;
+      case 'palace':
+        priceInput.placeholder = window.card.types.palace.minPrice;
+        priceInput.min = window.card.types.palace.minPrice;
+        break;
+      default:
+        priceInput.placeholder = window.card.types.bungalo.minPrice;
+        priceInput.min = window.card.types.bungalo.minPrice;
     }
-    capacityInput.value = roomValue;
   };
-  disableCapacityOptions(roomInputValue);
-};
-limitationCapacity();
+  limitationPrice();
 
-var onRoomNumberInputChange = function () {
+  var onTypeInputChange = function () {
+    limitationPrice();
+  };
+  typeInput.addEventListener('change', onTypeInputChange);
+
+  var syncTime = function (changeElem, repeatElem) {
+    repeatElem.value = changeElem.value;
+  };
+
+  var onTimeInInputChange = function () {
+    syncTime(timeInInput, timeOutInput);
+  };
+  timeInInput.addEventListener('change', onTimeInInputChange);
+
+  var onTimeOutInputChange = function () {
+    syncTime(timeOutInput, timeInInput);
+  };
+  timeOutInput.addEventListener('change', onTimeOutInputChange);
+
+  var limitationCapacity = function () {
+    var roomInputValue = roomNumberInput.value;
+    var capacityOptions = capacityInput.querySelectorAll('option');
+
+    var disableCapacityOptions = function (value) {
+      var HUNDRED_ROOMS = '100';
+      var NOT_FOR_GUESTS = '0';
+      var roomValue = value === HUNDRED_ROOMS ? NOT_FOR_GUESTS : value;
+      for (var j = 0; j < capacityOptions.length; j++) {
+        var flag = true;
+        if ((capacityOptions[j].value > NOT_FOR_GUESTS && capacityOptions[j].value <= roomValue) || (roomValue === NOT_FOR_GUESTS && capacityOptions[j].value === roomValue)) {
+          flag = false;
+        }
+        capacityOptions[j].disabled = flag;
+      }
+      capacityInput.value = roomValue;
+    };
+    disableCapacityOptions(roomInputValue);
+  };
   limitationCapacity();
-};
-roomNumberInput.addEventListener('change', onRoomNumberInputChange);
+
+  var onRoomNumberInputChange = function () {
+    limitationCapacity();
+  };
+  roomNumberInput.addEventListener('change', onRoomNumberInputChange);
+})();
